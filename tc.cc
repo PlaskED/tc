1e6eb186008f97b3a63071231b3e2ac355a73acc
Oscar Holm <oscho707@student.liu,se>
2016-12-07 02:12:34 +0100

    Restructured classes, WiP

--- tc.cc
+++ tc.cc
@@ -11,142 +11,110 @@
 
 using namespace std;
 
-const float modifier{1.495}; //prot stance and defiance modifier
-const float hs8{138+145}; //damage bonus + innate threat
-const float hs9{157+145}; //damage bonus + innate threat
 
-void Weapon::calculate_tps()
+void Thunderfury::calculate_ranges()
 {
-  cout << name << endl;
-  if (name == "Thunderfury, Blessed Blade of the Windseeker (buffed)") {
-    float procCha1 = 0.25;
-    float Procthreat1 = 270+92+149;
-    float procTPS1 = ((Procthreat1*modifier*procCha1)/speed); //threat per proc divided by speed and 0,25 multiplier as 25% procchance
-    float nrDMG1 = ((16+30)/2); //the extra NR dmg per hit calculated in weapon dps
-    //float nrDMG = 0; //Currently bugged on nost
-    float fullrot1=0.166666666666666*0.25*Procthreat1; //2/6 abilities in 6 sec rotrocs as SS doesnt proc
-    cout << "fullrot: " << fullrot1
-	 << " Procthreat: " << Procthreat1 
-	 << " procTPS: " << procTPS1 << endl;
-    
-    tpsr8_100 = (((nrDMG1+hs8)*modifier)/speed)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr9_100 = (((nrDMG1+hs9)*modifier)/speed)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr8_75 = ((((nrDMG1+hs8)*modifier)/speed)*0.75)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr9_75 = ((((nrDMG1+hs9)*modifier)/speed)*0.75)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr8_50 = ((((nrDMG1+hs8)*modifier)/speed)*0.50)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr9_50 = ((((nrDMG1+hs9)*modifier)/speed)*0.50)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr8_25 = ((((nrDMG1+hs8)*modifier)/speed)*0.25)+((dps+fullrot1)*modifier)+procTPS1;
-    tpsr9_25 = ((((nrDMG1+hs9)*modifier)/speed)*0.25)+((dps+fullrot1)*modifier)+procTPS1;
-  }
-  else if (name == "Thunderfury, Blessed Blade of the Windseeker")
-  {
-    float procCha = 0.15;
-    float Procthreat = 270+92+149;
-    float procTPS = ((Procthreat*modifier*procCha)/speed); //threat per proc divided by speed and 0,25 multiplier as 25% procchance
-    //float nrDMG{((16+30)/2)}; //the extra NR dmg per hit calculated in weapon dps
-    float nrDMG = 0; //Currently bugged on nost
-    float fullrot = 0.16666666666666666*0.20*Procthreat; //1/6 abilities in 6 sec rot procs as SS doesnt proc
-    cout << "fullrot: " << fullrot
-	 << " Procthreat: " << Procthreat 
-	 << " procTPS: " << procTPS << endl;
-    
-    tpsr8_100 = (((nrDMG+hs8)*modifier)/speed)+((dps+fullrot)*modifier)+procTPS;
-    tpsr9_100 = (((nrDMG+hs9)*modifier)/speed)+((dps+fullrot)*modifier)+procTPS;
-    tpsr8_75 = ((((nrDMG+hs8)*modifier)/speed)*0.75)+((dps+fullrot)*modifier)+procTPS;
-    tpsr9_75 = ((((nrDMG+hs9)*modifier)/speed)*0.75)+((dps+fullrot)*modifier)+procTPS;
-    tpsr8_50 = ((((nrDMG+hs8)*modifier)/speed)*0.50)+((dps+fullrot)*modifier)+procTPS;
-    tpsr9_50 = ((((nrDMG+hs9)*modifier)/speed)*0.50)+((dps+fullrot)*modifier)+procTPS;
-    tpsr8_25 = ((((nrDMG+hs8)*modifier)/speed)*0.25)+((dps+fullrot)*modifier)+procTPS;
-    tpsr9_25 = ((((nrDMG+hs9)*modifier)/speed)*0.25)+((dps+fullrot)*modifier)+procTPS;
-  }
-  else {
-    tpsr8_100 = ((hs8*modifier)/speed)+(dps*modifier);
-    tpsr9_100 = ((hs9*modifier)/speed)+(dps*modifier);
-    tpsr8_75 = (((hs8*modifier)/speed)*0.75)+(dps*modifier);
-    tpsr9_75 = (((hs9*modifier)/speed)*0.75)+(dps*modifier);
-    tpsr8_50 = (((hs8*modifier)/speed)*0.50)+(dps*modifier);
-    tpsr9_50 = (((hs9*modifier)/speed)*0.50)+(dps*modifier);
-    tpsr8_25 = (((hs8*modifier)/speed)*0.25)+(dps*modifier);
-    tpsr9_25 = (((hs9*modifier)/speed)*0.25)+(dps*modifier);
-  }
+    tpsMap["tpsr8_100"] = ((hs8*modifier)/speed)+(dps*modifier)+procTPS;
+    tpsMap["tpsr9_100"] = ((hs9*modifier)/speed)+(dps*modifier)+procTPS;
+    tpsMap["tpsr8_75"] = ((((hs8*modifier)/speed))*0.75)+(dps*modifier)+procTPS;
+    tpsMap["tpsr9_75"] = ((((hs9*modifier)/speed))*0.75)+(dps*modifier)+procTPS;
+    tpsMap["tpsr8_50"] = ((((hs8*modifier)/speed))*0.50)+(dps*modifier)+procTPS;
+    tpsMap["tpsr9_50"] = ((((hs9*modifier)/speed))*0.50)+(dps*modifier)+procTPS;
+    tpsMap["tpsr8_25"] = ((((hs8*modifier)/speed))*0.25)+(dps*modifier)+procTPS;
+    tpsMap["tpsr9_25"] = ((((hs9*modifier)/speed))*0.25)+(dps*modifier)+procTPS;
 }
 
-void print_weapon(const Weapon wep)
+void Weapon::calculate_ranges()
 {
-  cout << "-------------------------------------" << endl
-       << wep.name << endl 
-       << "DPS: " << wep.dps
-       << endl << "Speed: "
-       << wep.speed << endl
-       << "100% HS: " << "R8: "
-       << wep.tpsr8_100 << "  R9: " << wep.tpsr9_100 << endl
-       << "75% HS:  " << "R8: "
-       << wep.tpsr8_75 << "  R9: " << wep.tpsr9_75 << endl
-       << "50% HS:  " << "R8: "
-       << wep.tpsr8_50 << "  R9: " << wep.tpsr9_50 << endl
-       << "25% HS:  " << "R8: "
-       << wep.tpsr8_25 << "  R9: " << wep.tpsr9_25 << endl;
+    tpsMap["tpsr8_100"] = ((hs8*modifier)/speed)+(dps*modifier);
+    tpsMap["tpsr9_100"] = ((hs9*modifier)/speed)+(dps*modifier);
+    tpsMap["tpsr8_75"] = ((((hs8*modifier)/speed))*0.75)+(dps*modifier);
+    tpsMap["tpsr9_75"] = ((((hs9*modifier)/speed))*0.75)+(dps*modifier);
+    tpsMap["tpsr8_50"] = ((((hs8*modifier)/speed))*0.50)+(dps*modifier);
+    tpsMap["tpsr9_50"] = ((((hs9*modifier)/speed))*0.50)+(dps*modifier);
+    tpsMap["tpsr8_25"] = ((((hs8*modifier)/speed))*0.25)+(dps*modifier);
+    tpsMap["tpsr9_25"] = ((((hs9*modifier)/speed))*0.25)+(dps*modifier);
 }
 
-void print_map_sorted(const map<int,Weapon*> &weplist)
+void print_weapon(Weapon* wep)
 {
-  //Prints out the weaponlist sorted after the values from pair1 and pair2. 
-  vector<pair<int,Weapon*>> sorted_weplist {weplist.begin(), weplist.end()};
+    cout << "-------------------------------------" << endl
+	 << wep->name << endl << "DPS: " << wep->dps
+	 << endl << "Speed: " << wep->speed << endl
+	 << "100% HS: " << "R8: " << wep->tpsMap["tpsr8_100"] 
+	 << "  R9: " << wep->tpsMap["tpsr9_100"] << endl
+	 << "75% HS:  " << "R8: " << wep->tpsMap["tpsr8_75"]
+	 << "  R9: " << wep->tpsMap["tpsr9_75"] << endl
+	 << "50% HS:  " << "R8: " << wep->tpsMap["tpsr8_50"] 
+	 << "  R9: " << wep->tpsMap["tpsr9_50"] << endl
+	 << "25% HS:  " << "R8: " << wep->tpsMap["tpsr8_25"] 
+	 << "  R9: " << wep->tpsMap["tpsr9_25"] << endl;
+}
 
-  auto sort_lambda = [] (pair<int,Weapon*> pair1, pair<int,Weapon*> pair2)->bool
-    {return pair1.second->tpsr8_100 > pair2.second->tpsr8_100;};
+void print_map_sorted(const map<int,Weapon*> &weplist, const string sortBy)
+{
+    //Prints out the weaponlist sorted after the values from pair1 and pair2.
+    cout << "Sorted by field: " << sortBy << endl;
+    vector<pair<int,Weapon*>> sorted_weplist {weplist.begin(), weplist.end()};
 
-  sort(sorted_weplist.begin(), sorted_weplist.end(), sort_lambda);
-  for (pair<int,Weapon*> pair_ : sorted_weplist)
-  {
-    print_weapon(*pair_.second);
-  }
+    auto sort_lambda = [] (pair<int,Weapon*> pair1, pair<int,Weapon*> pair2)->bool
+	{return pair1.second->tpsMap[sortBy] > pair2.second->tpsMap[sortBy];};
+
+    sort(sorted_weplist.begin(), sorted_weplist.end(), sort_lambda);
+    for (pair<int,Weapon*> pair_ : sorted_weplist)
+    {
+	print_weapon(*pair_.second);
+    }
 }
 
 int main()
 { 
-  string filename;
-  ifstream file;
-  stringstream ss;
-  string line,wepname;
-  map<int,Weapon*> wepmap;
-  int i{0};
+    string filename;
+    ifstream file;
+    stringstream ss;
+    string line;
+    map<int,Weapon*> wepmap;
+    float name, dps, speed;
+    int i{0};
 
-  cout << "- Weapon TPS calculator - " << endl
-       << "Filename: ";
+    cout << "- Weapon TPS calculator - " << endl
+	 << "Filename: ";
   
-  cin >> filename;
-  file.open(filename);
-  if (!file.is_open())
-  {
-    cerr << "'" << filename
-         << "' could not be opened" << endl;
-    return 1;
-  }
-  cin.clear();
-  
-
-  while(getline(file,wepname))
-  {
-    if (wepname != "-")
+    cin >> filename;
+    file.open(filename);
+    if (!file.is_open())
     {
-      wepmap[i] = new Weapon;
-      wepmap[i]->name = wepname;
-
-      getline(file,line);
-      wepmap[i]->dps = stof(line);
-
-      getline(file,line);
-      wepmap[i]->speed = stof(line);
-      wepmap[i]->calculate_tps();
-      //print_weapon(*wepmap[i]);
+	cerr << "'" << filename
+	     << "' could not be opened" << endl;
+	return 1;
     }
-    ss.str("");
-    ss.clear();
-    i++;
-  }
-    
-  print_map_sorted(wepmap);
+    cin.clear();
+  
 
-  return 0;
+    while(getline(file, name))
+    {
+	if (name != "-")
+	{
+	    getline(file,line);
+	    dps = float(line);
+
+	    getline(file,line);
+	    speed = float(line);
+
+	    wepmap[i] = new Weapon(name, dps, speed);
+	    wepmap[i]->calculate_ranges();
+	}
+	ss.str("");
+	ss.clear();
+	i++;
+    }
+
+    wepmap[i] = new Thunderfury("Thunderfury, Blessed Blade of the Windseeker", 
+				53.9, 1.90, true, false, true);
+    wepmap[i]->calculate_ranges();
+    
+    
+    print_map_sorted(wepmap, "tpsr8_100");
+
+    return 0;
 }
--- tcheader.h
+++ tcheader.h
@@ -1,17 +1,36 @@
+const float modifier{1.495}; //prot stance and defiance modifier
+const float hs8{138+145}; //damage bonus + innate threat
+const float hs9{157+145}; //damage bonus + innate threat
+
 class Weapon 
 {
  public:
-  std::string name;
-  float dps,speed,tpsr8_100,tpsr9_100,tpsr8_75,tpsr9_75,tpsr8_50,tpsr9_50,tpsr8_25,tpsr9_25;
+ Weapon(std::string n, float d, float s) : name(n), dps(d), speed(s) {}
+
+    std::map<std::string, float> tpsMap = {
+	{"tpsr8_100",0.0}, {"tpsr9_100",0.0},{"tpsr8_75",0.0},{"tpsr9_75",0.0},
+	{"tpsr8_50",0.0},{"tpsr9_50",0.0},{"tpsr8_25",0.0},{"tpsr9_25",0.0} };
+    std::string name;
+    float dps;
+    float speed;
+    void calculate_ranges();
   
-  void calculate_tps();
 };
 
-class Thunderfury: public Weapon
+class Thunderfury : public Weapon
 {
  public:
-  std::string server;
-  const float autoProcRate{0.25};
-  std::map <std::string,int> ProcThreat = { };
-  
+ Thunderfury(std::string n, float d, float s, bool hasNr, bool hass, bool hasr) 
+     : Weapon(name, dps, speed), hasNrdmg(hasNr), hasSlam(hass), hasRev(hasr) {}
+    std::string server;
+    float autoProcRate{0.25};
+    float procThreat{270+92+149};
+    bool hasNrdmg;
+    bool hasSlam;
+    bool hasRev;
+    float nrTPS = (float(hasNrdmg)*((16+30)/2))*(1/speed);
+    float rotTPS = ((float(hasSlam)+float(hasRev))*0.16666666666666666)*(autoProcRate*procThreat); //TPS from rotation procs, rot is 6 sec
+    float procTPS = ((((procThreat*autoProcRate)+rotTPS+nrTPS)*modifier)/speed);
+
+    void calculate_ranges();
 };
