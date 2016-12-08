24695aae30932b1f09f731a87aceacc5a76622ff
Oscar Holm <oscho707@student.liu,se>
2016-12-08 21:51:45 +0100

    Restructured some functions, fixed typo in printout.

--- tc.cc
+++ tc.cc
@@ -26,6 +26,7 @@ void Weapon::calculate_ranges()
 
     for ( auto it = range.rbegin() ; it != range.rend(); ++it) {
 	int ra = *it;
+	cout << procTPS << endl;
 	float r8 = (((hs8*modifier)/speed)*(ra/100.0))+(dps*modifier)+procTPS;
 	float r9 = (((hs9*modifier)/speed)*(ra/100.0))+(dps*modifier)+procTPS;
 	sec.first = r8;
@@ -43,16 +44,14 @@ void print_weapon(Weapon* wep)
 	 << " - Speed: " << wep->speed << endl;
 
     for ( pair<int, pair<float,float>> pair_ : wep->tpsVec) {
-	cout << left << setw(3) << pair_.first << "% HS"
+	cout << "| " << left << setw(3) << pair_.first << " %HS"
 	     << " | R8: " << setw(7) << pair_.second.first
-	     << " | R9: " << setw(7) << pair_.second.first << endl;
-
+	     << " | R9: " << setw(7) << pair_.second.second << " |" << endl;
     }
 }
 
-void print_vec_sorted(const vector<Weapon*> &weplist, const int sortBy, bool r9)
+void sort_vec(vector<Weapon*> &weplist, const int sortBy, bool r9)
 {
-    //Prints out the weaponlist sorted after the values from pair1 and pair2.
     cout << "Sorted by field: " << sortBy << endl;
     vector<Weapon*> sorted_weplist {weplist.begin(), weplist.end()};
     auto sort_lambda = [sortBy] (Weapon* wep1, Weapon* wep2)->bool
@@ -66,7 +65,12 @@ void print_vec_sorted(const vector<Weapon*> &weplist, const int sortBy, bool r9)
     }
 
     sort(sorted_weplist.begin(), sorted_weplist.end(), sort_lambda);
-    for (Weapon* wep : sorted_weplist)
+    weplist = sorted_weplist;
+}
+
+void print_vec(const vector<Weapon*> weplist)
+{
+    for (Weapon* wep : weplist)
     {
 	print_weapon(wep);
     }
@@ -118,8 +122,8 @@ int main()
     wepvec.push_back(new Thunderfury("Thunderfury, Blessed Blade of the Windseeker", "Nostalrius", 53.90, 1.90, true, false, true));
     wepvec[wepvec.size()-1]->calculate_ranges();
     
-    
-    print_vec_sorted(wepvec, 0, false);
+    sort_vec(wepvec, 0, false);
+    print_vec(wepvec);
 
     return 0;
 }
