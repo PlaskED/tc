1d076e5c1b507730629cdf1c77f6113f7d59cb82
Oscar Holm <oscho707@student.liu,se>
2016-12-08 15:51:46 +0100

    More changes to classes. Simplified and more generalized.

--- tc.cc
+++ tc.cc
@@ -11,34 +11,47 @@
 
 using namespace std;
 
+const string delimiter{"_"};
+
 
 void Thunderfury::calculate_ranges()
 {
-    tpsMap["tpsr8_100"] = ((hs8*modifier)/speed)+(dps*modifier)+procTPS;
-    tpsMap["tpsr9_100"] = ((hs9*modifier)/speed)+(dps*modifier)+procTPS;
-    tpsMap["tpsr8_75"] = ((((hs8*modifier)/speed))*0.75)+(dps*modifier)+procTPS;
-    tpsMap["tpsr9_75"] = ((((hs9*modifier)/speed))*0.75)+(dps*modifier)+procTPS;
-    tpsMap["tpsr8_50"] = ((((hs8*modifier)/speed))*0.50)+(dps*modifier)+procTPS;
-    tpsMap["tpsr9_50"] = ((((hs9*modifier)/speed))*0.50)+(dps*modifier)+procTPS;
-    tpsMap["tpsr8_25"] = ((((hs8*modifier)/speed))*0.25)+(dps*modifier)+procTPS;
-    tpsMap["tpsr9_25"] = ((((hs9*modifier)/speed))*0.25)+(dps*modifier)+procTPS;
+    vector<int> range = {25,50,75,100};
+    for (int i=0 ; i < range.size() ; i++) {
+	tpsMap["tpsr8_"+to_string(range[i])] = (((hs8*modifier)/speed)*(range[i]/100.0))+(dps*modifier)+procTPS;
+	tpsMap["tpsr9_"+to_string(range[i])] = (((hs9*modifier)/speed)*(range[i]/100.0))+(dps*modifier)+procTPS;
+    }
 }
 
 void Weapon::calculate_ranges()
 {
-    tpsMap["tpsr8_100"] = ((hs8*modifier)/speed)+(dps*modifier);
-    tpsMap["tpsr9_100"] = ((hs9*modifier)/speed)+(dps*modifier);
-    tpsMap["tpsr8_75"] = ((((hs8*modifier)/speed))*0.75)+(dps*modifier);
-    tpsMap["tpsr9_75"] = ((((hs9*modifier)/speed))*0.75)+(dps*modifier);
-    tpsMap["tpsr8_50"] = ((((hs8*modifier)/speed))*0.50)+(dps*modifier);
-    tpsMap["tpsr9_50"] = ((((hs9*modifier)/speed))*0.50)+(dps*modifier);
-    tpsMap["tpsr8_25"] = ((((hs8*modifier)/speed))*0.25)+(dps*modifier);
-    tpsMap["tpsr9_25"] = ((((hs9*modifier)/speed))*0.25)+(dps*modifier);
+    vector<int> range = {25,50,75,100};
+    for (int i=0 ; i < range.size() ; i++) {
+	tpsMap["tpsr8_"+to_string(range[i])] = (((hs8*modifier)/speed)*(range[i]/100.0))+(dps*modifier);
+	tpsMap["tpsr9_"+to_string(range[i])] = (((hs9*modifier)/speed)*(range[i]/100.0))+(dps*modifier);
+    }
+
 }
 
 void print_weapon(Weapon* wep)
 {
     cout << "-------------------------------------" << endl
+         << wep->name << endl << "DPS: " << wep->dps
+	 << endl << "Speed: " << wep->speed << endl;
+
+    for (pair<string,float> pair_ : wep->tpsMap) {
+	string s = pair_.first;
+	string token = s.substr(6, s.rfind(delimiter));
+	char rank = s[4];
+	
+	
+	cout << token << "% HS R" << rank << ": "  << pair_.second;
+	if (rank == '9') {
+	    cout << endl;
+	}
+
+    }
+    /*cout << "-------------------------------------" << endl
 	 << wep->name << endl << "DPS: " << wep->dps
 	 << endl << "Speed: " << wep->speed << endl
 	 << "100% HS: " << "R8: " << wep->tpsMap["tpsr8_100"] 
@@ -49,6 +62,7 @@ void print_weapon(Weapon* wep)
 	 << "  R9: " << wep->tpsMap["tpsr9_50"] << endl
 	 << "25% HS:  " << "R8: " << wep->tpsMap["tpsr8_25"] 
 	 << "  R9: " << wep->tpsMap["tpsr9_25"] << endl;
+    */
 }
 
 void print_map_sorted(const map<int,Weapon*> &weplist, const string sortBy)
@@ -57,13 +71,13 @@ void print_map_sorted(const map<int,Weapon*> &weplist, const string sortBy)
     cout << "Sorted by field: " << sortBy << endl;
     vector<pair<int,Weapon*>> sorted_weplist {weplist.begin(), weplist.end()};
 
-    auto sort_lambda = [] (pair<int,Weapon*> pair1, pair<int,Weapon*> pair2)->bool
+    auto sort_lambda = [sortBy] (pair<int,Weapon*> pair1, pair<int,Weapon*> pair2)->bool
 	{return pair1.second->tpsMap[sortBy] > pair2.second->tpsMap[sortBy];};
 
     sort(sorted_weplist.begin(), sorted_weplist.end(), sort_lambda);
     for (pair<int,Weapon*> pair_ : sorted_weplist)
     {
-	print_weapon(*pair_.second);
+	print_weapon(pair_.second);
     }
 }
 
@@ -72,15 +86,16 @@ int main()
     string filename;
     ifstream file;
     stringstream ss;
-    string line;
+    string line, name;
     map<int,Weapon*> wepmap;
-    float name, dps, speed;
+    float dps, speed;
     int i{0};
 
     cout << "- Weapon TPS calculator - " << endl
 	 << "Filename: ";
   
-    cin >> filename;
+    //cin >> filename;
+    filename = "weplist.tc";
     file.open(filename);
     if (!file.is_open())
     {
@@ -91,15 +106,15 @@ int main()
     cin.clear();
   
 
-    while(getline(file, name))
+    while(getline(file, name) && i < 10)
     {
 	if (name != "-")
 	{
 	    getline(file,line);
-	    dps = float(line);
+	    dps = stof(line);
 
 	    getline(file,line);
-	    speed = float(line);
+	    speed = stof(line);
 
 	    wepmap[i] = new Weapon(name, dps, speed);
 	    wepmap[i]->calculate_ranges();
@@ -109,8 +124,8 @@ int main()
 	i++;
     }
 
-    wepmap[i] = new Thunderfury("Thunderfury, Blessed Blade of the Windseeker", 
-				53.9, 1.90, true, false, true);
+    wepmap[i] = new Thunderfury("Thunderfury, Blessed Blade of the Windseeker",
+				"Nostalrius", 53.9, 1.90, true, false, true);
     wepmap[i]->calculate_ranges();
     
     
--- tcheader.h
+++ tcheader.h
@@ -1,27 +1,31 @@
+#ifndef TCHEADER_H // header guards
+#define TCHEADER_H
+#include <string>
+#include <map>
+
 const float modifier{1.495}; //prot stance and defiance modifier
 const float hs8{138+145}; //damage bonus + innate threat
 const float hs9{157+145}; //damage bonus + innate threat
 
-class Weapon 
+class Weapon
 {
  public:
- Weapon(std::string n, float d, float s) : name(n), dps(d), speed(s) {}
-
-    std::map<std::string, float> tpsMap = {
-	{"tpsr8_100",0.0}, {"tpsr9_100",0.0},{"tpsr8_75",0.0},{"tpsr9_75",0.0},
-	{"tpsr8_50",0.0},{"tpsr9_50",0.0},{"tpsr8_25",0.0},{"tpsr9_25",0.0} };
+ Weapon(std::string n, float d, float s) : name(n), dps(d), speed(s) {};
+    
+    std::map<std::string, float> tpsMap;
     std::string name;
     float dps;
     float speed;
-    void calculate_ranges();
+
+    virtual void calculate_ranges();
   
 };
 
 class Thunderfury : public Weapon
 {
  public:
- Thunderfury(std::string n, float d, float s, bool hasNr, bool hass, bool hasr) 
-     : Weapon(name, dps, speed), hasNrdmg(hasNr), hasSlam(hass), hasRev(hasr) {}
+ Thunderfury(std::string name, std::string serv, float dps, float speed, bool hasNr, bool hass, bool hasr) : Weapon(name, dps, speed) {};
+
     std::string server;
     float autoProcRate{0.25};
     float procThreat{270+92+149};
@@ -34,3 +38,5 @@ class Thunderfury : public Weapon
 
     void calculate_ranges();
 };
+
+#endif
