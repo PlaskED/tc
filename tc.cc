896b1059eab2ffd86d4dfbdeb421effed6bf85ce
Oscar Holm <oscho707@student.liu,se>
2016-12-08 20:31:27 +0100

    Changed all maps to vectors.

--- tc.cc
+++ tc.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <map>
 #include <vector>
 #include <sstream>
 #include <fstream>
@@ -11,73 +10,65 @@
 
 using namespace std;
 
-const string delimiter{"_"};
+const string delimiter{"Thunderfury"};
 
-
-void Thunderfury::calculate_ranges()
-{
-    vector<int> range = {25,50,75,100};
-    for (int i=0 ; i < range.size() ; i++) {
-	tpsMap["tpsr8_"+to_string(range[i])] = (((hs8*modifier)/speed)*(range[i]/100.0))+(dps*modifier)+procTPS;
-	tpsMap["tpsr9_"+to_string(range[i])] = (((hs9*modifier)/speed)*(range[i]/100.0))+(dps*modifier)+procTPS;
-    }
-}
 
 void Weapon::calculate_ranges()
 {
-    vector<int> range = {25,50,75,100};
-    for (int i=0 ; i < range.size() ; i++) {
-	tpsMap["tpsr8_"+to_string(range[i])] = (((hs8*modifier)/speed)*(range[i]/100.0))+(dps*modifier);
-	tpsMap["tpsr9_"+to_string(range[i])] = (((hs9*modifier)/speed)*(range[i]/100.0))+(dps*modifier);
+    pair<int, pair<float,float>> val;
+    pair<float,float> sec;
+    const vector<int> range = {25,50,75,100};
+
+    string token = name.substr(0, name.find(delimiter));
+    if (token.length() > 1) {
+	calc_proc();
     }
 
+    for ( auto it = range.rbegin() ; it != range.rend(); ++it) {
+	int ra = *it;
+	float r8 = (((hs8*modifier)/speed)*(ra/100.0))+(dps*modifier)+procTPS;
+	float r9 = (((hs9*modifier)/speed)*(ra/100.0))+(dps*modifier)+procTPS;
+	sec.first = r8;
+	sec.second = r9;
+	val.first = ra;
+	val.second = sec;
+	tpsVec.push_back(val);
+    }
 }
 
 void print_weapon(Weapon* wep)
 {
     cout << "-------------------------------------" << endl
          << wep->name << endl << "DPS: " << wep->dps
-	 << endl << "Speed: " << wep->speed << endl;
+	 << " - Speed: " << wep->speed << endl;
 
-    for (pair<string,float> pair_ : wep->tpsMap) {
-	string s = pair_.first;
-	string token = s.substr(6, s.rfind(delimiter));
-	char rank = s[4];
-	
-	
-	cout << token << "% HS R" << rank << ": "  << pair_.second;
-	if (rank == '9') {
-	    cout << endl;
-	}
+    for ( pair<int, pair<float,float>> pair_ : wep->tpsVec) {
+	cout << left << setw(3) << pair_.first << "% HS"
+	     << " | R8: " << setw(7) << pair_.second.first
+	     << " | R9: " << setw(7) << pair_.second.first << endl;
 
     }
-    /*cout << "-------------------------------------" << endl
-	 << wep->name << endl << "DPS: " << wep->dps
-	 << endl << "Speed: " << wep->speed << endl
-	 << "100% HS: " << "R8: " << wep->tpsMap["tpsr8_100"] 
-	 << "  R9: " << wep->tpsMap["tpsr9_100"] << endl
-	 << "75% HS:  " << "R8: " << wep->tpsMap["tpsr8_75"]
-	 << "  R9: " << wep->tpsMap["tpsr9_75"] << endl
-	 << "50% HS:  " << "R8: " << wep->tpsMap["tpsr8_50"] 
-	 << "  R9: " << wep->tpsMap["tpsr9_50"] << endl
-	 << "25% HS:  " << "R8: " << wep->tpsMap["tpsr8_25"] 
-	 << "  R9: " << wep->tpsMap["tpsr9_25"] << endl;
-    */
 }
 
-void print_map_sorted(const map<int,Weapon*> &weplist, const string sortBy)
+void print_vec_sorted(const vector<Weapon*> &weplist, const int sortBy, bool r9)
 {
     //Prints out the weaponlist sorted after the values from pair1 and pair2.
     cout << "Sorted by field: " << sortBy << endl;
-    vector<pair<int,Weapon*>> sorted_weplist {weplist.begin(), weplist.end()};
+    vector<Weapon*> sorted_weplist {weplist.begin(), weplist.end()};
+    auto sort_lambda = [sortBy] (Weapon* wep1, Weapon* wep2)->bool
+	    {return wep1->tpsVec[sortBy].second.first 
+	     > wep2->tpsVec[sortBy].second.first;};
 
-    auto sort_lambda = [sortBy] (pair<int,Weapon*> pair1, pair<int,Weapon*> pair2)->bool
-	{return pair1.second->tpsMap[sortBy] > pair2.second->tpsMap[sortBy];};
+    if (r9 == true) {
+	auto sort_lambda = [sortBy] (Weapon* wep1, Weapon* wep2)->bool
+	    {return wep1->tpsVec[sortBy].second.second 
+	     > wep2->tpsVec[sortBy].second.second;};
+    }
 
     sort(sorted_weplist.begin(), sorted_weplist.end(), sort_lambda);
-    for (pair<int,Weapon*> pair_ : sorted_weplist)
+    for (Weapon* wep : sorted_weplist)
     {
-	print_weapon(pair_.second);
+	print_weapon(wep);
     }
 }
 
@@ -87,7 +78,7 @@ int main()
     ifstream file;
     stringstream ss;
     string line, name;
-    map<int,Weapon*> wepmap;
+    vector<Weapon*> wepvec;
     float dps, speed;
     int i{0};
 
@@ -106,7 +97,7 @@ int main()
     cin.clear();
   
 
-    while(getline(file, name) && i < 10)
+    while(getline(file, name) && i < 0)
     {
 	if (name != "-")
 	{
@@ -115,21 +106,20 @@ int main()
 
 	    getline(file,line);
 	    speed = stof(line);
-
-	    wepmap[i] = new Weapon(name, dps, speed);
-	    wepmap[i]->calculate_ranges();
+	    
+	    wepvec.push_back(new Weapon(name, dps, speed));
+	    wepvec[wepvec.size()-1]->calculate_ranges();
 	}
 	ss.str("");
 	ss.clear();
 	i++;
     }
 
-    wepmap[i] = new Thunderfury("Thunderfury, Blessed Blade of the Windseeker",
-				"Nostalrius", 53.9, 1.90, true, false, true);
-    wepmap[i]->calculate_ranges();
+    wepvec.push_back(new Thunderfury("Thunderfury, Blessed Blade of the Windseeker", "Nostalrius", 53.90, 1.90, true, false, true));
+    wepvec[wepvec.size()-1]->calculate_ranges();
     
     
-    print_map_sorted(wepmap, "tpsr8_100");
+    print_vec_sorted(wepvec, 0, false);
 
     return 0;
 }
--- tcheader.h
+++ tcheader.h
@@ -1,7 +1,5 @@
 #ifndef TCHEADER_H // header guards
 #define TCHEADER_H
-#include <string>
-#include <map>
 
 const float modifier{1.495}; //prot stance and defiance modifier
 const float hs8{138+145}; //damage bonus + innate threat
@@ -12,12 +10,14 @@ class Weapon
  public:
  Weapon(std::string n, float d, float s) : name(n), dps(d), speed(s) {};
     
-    std::map<std::string, float> tpsMap;
+    std::vector<std::pair<int, std::pair<float,float>>> tpsVec;
     std::string name;
     float dps;
     float speed;
+    float procTPS{0};
 
-    virtual void calculate_ranges();
+    virtual void calc_proc() {procTPS=0;};
+    void calculate_ranges();
   
 };
 
@@ -34,7 +34,9 @@ class Thunderfury : public Weapon
     bool hasRev;
     float nrTPS = (float(hasNrdmg)*((16+30)/2))*(1/speed);
     float rotTPS = ((float(hasSlam)+float(hasRev))*0.16666666666666666)*(autoProcRate*procThreat); //TPS from rotation procs, rot is 6 sec
-    float procTPS = ((((procThreat*autoProcRate)+rotTPS+nrTPS)*modifier)/speed);
+    void calc_proc() {
+	procTPS = ((((procThreat*autoProcRate)+rotTPS+nrTPS)*modifier)/speed);
+    };
 
     void calculate_ranges();
 };
